1dd427715515f572c72d69cbae0cce0cacb01836
prasanth8961 <prasanthprs143@gmail.com>
2025-08-19 11:48:23 +0530

    FIX: proxy

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:8080",
-          changeOrigin: true,
-        },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
       },
     },
   },
