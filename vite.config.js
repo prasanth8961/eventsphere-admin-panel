2c70fb1b05aa447a2ad6a7926f2092237667d028
prasanth8961 <prasanthprs143@gmail.com>
2025-09-06 15:55:32 +0530

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080",
-        // target: "https://eventsphere-backend-3.onrender.com",
+        // target: "http://127.0.0.1:8080",
+        target: "https://eventsphere-backend-3.onrender.com",
         changeOrigin: true,
       },
     },
